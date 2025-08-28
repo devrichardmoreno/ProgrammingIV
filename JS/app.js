5ed7032d1f75000460e62e6bb78c4a09a3a2c03f
Richard <richardmorenoar@gmail.com>
2025-08-28 09:28:25 -0300

    Realizando la parte 2

--- JS/app.js
+++ JS/app.js
@@ -137,13 +137,45 @@ function showClientsWithProducts(){
     clients.forEach(n => console.log(n.name + " "+ n.products))
 }
 
-showClientsWithProducts()
+//PARTE 2 
+
+//ejercicio 7 
+function procesarClientes(clientes,callback){
+    console.log("procesando callbacks")
+    callback(clientes);
+}
+
+function showNames(clients){
+    clients.forEach(n => console.log(n.name))
+}
+
+function precioTotal(clients) {
+    let sum =  productos.reduce((a,b) => ab.price,0)
+    console.log(sum)
+}
 
 
+//procesarClientes(clients,showNames)
 
+//procesarClientes(clients,precioTotal)
 
+//ejercicio 8 
 
+let numbers = [10,23,43,4,99,16,47,28,59,10]
 
+console.log(numbers.sort((a,b) => a-b))
 
+console.log(numbers.reverse((a,b) => a-b))
 
+//ejercicio 9 
+
+let tienda = {
+    productos: [],
+    sell: function(idProducto,cantidad){
+       let productoAEncontrar = productos.find(n => n.id===idProducto)
+        if(productoAEncontrar=!null){
+            productoAEncontrar.stock()
+        }
+    }
+}
 
